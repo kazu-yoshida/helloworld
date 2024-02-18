973b42dbbfb1902d9170731a6c0ee8f232e0c821
kazu-yoshida <32302897+kazu-yoshida@users.noreply.github.com>
2024-02-18 22:50:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,38 +12,49 @@ import streamlit as st
 #      st.write('Goodbye')
 
 # Day5
-import numpy as np
-import altair as alt
-import pandas as pd
+# import numpy as np
+# import altair as alt
+# import pandas as pd
+# import streamlit as st
+
+# st.header('st.write')
+
+# # Example 1
+
+# st.write('Hello, *World!* :sunglasses:')
+
+# # Example 2
+
+# st.write(1234)
+
+# # Example 3
+
+# df = pd.DataFrame({
+#      'first column': [1, 2, 3, 4],
+#      'second column': [10, 20, 30, 40]
+#      })
+# st.write(df)
+
+# # Example 4
+
+# st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# # Example 5
+
+# df2 = pd.DataFrame(
+#      np.random.randn(200, 3),
+#      columns=['a', 'b', 'c'])
+# c = alt.Chart(df2).mark_circle().encode(
+#      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+# st.write(c)
+
+# Day15
 import streamlit as st
 
-st.header('st.write')
+st.header('st.latex')
 
-# Example 1
-
-st.write('Hello, *World!* :sunglasses:')
-
-# Example 2
-
-st.write(1234)
-
-# Example 3
-
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
-
-# Example 4
-
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# Example 5
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
